14a4dea59751cfc625758e9094dd9e0e22077d99
Siddharth goyal <siddharthgoyal198@gmail.com>
2023-10-30 22:26:16 -0700

    Edits

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -35,119 +35,49 @@ const Skills = ({ darkMode }) => {
               className=" text-gray-700 font-medium w-[100%]"
             >
               Here are my skills.
-            </p>
-            {/* left box
-            <div
-              // data-aos="zoom-in"
-              // className="progress flex items-center h-[100%] justify-end md:justify-center"
-            >
-              {/* <div className=" flex flex-col gap-6  w-3/4  my-5 md:w-[90%]"> */}
-                {/* <ProgressBar logo={<SiPython />} name={"Python"} value={95} />
-                <ProgressBar logo={<IoLogoCss3 />} name={"CSS"} value={88} />
-                <ProgressBar
-                  logo={<SiJavascript />}
-                  name={"Javascript"}
-                  value={80}
-                />
-                <ProgressBar logo={<FaReact />} name={"React Js"} value={80} />
-                <ProgressBar
-                  logo={<SiTailwindcss />}
-                  name={"Tailwind CSS"}
-                  value={80} */}
-                {/* /> */}
-              {/* </div> */}
+            </p>           
             </div>
-          {/* </div>  */}
-          {/* right box */}
+          
+          
           {/* right relative flex-1 flex flex-wrap p-5 gap-10 items-center justify-center sm:w-full */}
           <div className="middle relative flex-1 flex flex-wrap p-5 gap-10 items-center justify-center sm:w-full">
-            <div className="first2 flex flex-col gap-10">
-              <SkillBox
-                logo={<IoLogoNodejs />}
-                black={"white"}
-                white={"black"}
-                skill={"Node Js"}
-              />
-              <SkillBox
-                logo={<SiMongodb />}
-                black={"white"}
-                white={"black"}
-                skill={"MongoDB"}
-              />
-            </div>
-            <div className="last2 flex flex-col gap-10">
-              <SkillBox
-                logo={<SiExpress />}
-                black={"black"}
-                white={"white"}
-                skill={"Express Js"}
-              />
-              <SkillBox
-                className=""
-                logo={
-                  <GrCode/>
-                }
-                black={"black"}
-                white={"white"}
-                skill={"C++"}
-              />
-              {/* <SkillBox
-                className=""
-                logo={
-                  <SiNextdotjs className=" text-white bg-black rounded-full h-fit border-white overflow-hidden" />
-                }
-                black={"black"}
-                white={"white"}
-                skill={"Next Js"}
-              /> */}
-            </div>
-          </div>
+  <div className="first2 flex flex-col gap-10 items-center justify-center"> {/* Added 'items-center' and 'justify-center' here */}
+    <SkillBox
+      logo={<IoLogoNodejs />}
+      black={"white"}
+      white={"black"}
+      skill={"Node Js"}
+    />
+    <SkillBox
+      logo={<SiMongodb />}
+      black={"white"}
+      white={"black"}
+      skill={"MongoDB"}
+    />
+  </div>
+  <div className="last2 flex flex-col gap-10 items-center justify-center"> {/* Added 'items-center' and 'justify-center' here */}
+    <SkillBox
+      logo={<SiExpress />}
+      black={"black"}
+      white={"white"}
+      skill={"Express Js"}
+    />
+    <SkillBox
+      className=""
+      logo={
+        <GrCode />
+      }
+      black={"black"}
+      white={"white"}
+      skill={"C++"}
+    />
+  </div>
+</div>
+
         </div>
 
         {/* icons */}
-        {/* <div
-          data-aos="fade-up"
-          data-aos-duration="1200"
-          className=" container m-auto flex items-center justify-center mt-8 gap-8 md:gap-5"
-        >
-          <img
-            className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]"
-            src="https://img.icons8.com/ios-filled/50/null/c-plus-plus-logo.png"
-          />
-
-          <img
-            className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]"
-            src="https://img.icons8.com/color/48/null/python--v1.png"
-          />
-          <img
-            className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]"
-            src="https://img.icons8.com/ios/50/null/react-native--v1.png"
-          />
-
-          <img
-            className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]"
-            src="https://img.icons8.com/color/48/null/sass.png"
-            />
-          <img
-            className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]"
-            src="https://img.icons8.com/ios-filled/50/null/git.png"
-            />
-            <img
-              className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]"
-              src="https://img.icons8.com/windows/50/null/sass--v1.png"
-            />
-          <img
-            className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]"
-            src="https://img.icons8.com/color/48/null/c-plus-plus-logo.png"
-          />
-          <img
-            className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]"
-            src="https://img.icons8.com/ios-filled/50/null/python.png"
-          />
-          <img className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]" src="https://img.icons8.com/color/48/null/javascript--v1.png"/>
-          <img className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]" src="https://img.icons8.com/color/48/null/nodejs.png"/>
-          <img className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]" src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/null/external-mongodb-a-cross-platform-document-oriented-database-program-logo-shadow-tal-revivo.png"/>
-        </div> */}
+        
       </div>
     </div>
   );
