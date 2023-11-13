5a370c37eaa29d75eb8f0bf536c15ee72dc22a2c
Siddharth goyal <siddharthgoyal198@gmail.com>
2023-11-13 13:20:15 -0700

    Bug Fix

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Resume = () => {
   return (
-    <div id="resume" className="container m-auto mt-16 flex flex-wrap">
+    <div id="resume" className=" container m-auto   mt-16">
       {/* heading */}
-      <div data-aos="fade-up" className="w-full mb-5">
+      <div data-aos="fade-up" className="relative mb-5">
         <h3 className=" text-3xl font-black text-gray-400 sm:text-2xl">
           Resume
         </h3>
-        <span className="h-[1.1px] w-[92%] bg-gray-300 block"></span>
+        <span className="h-[1.1px] right-0 absolute w-[92%] bg-gray-300 block"></span>
       </div>
       <div data-aos="fade-up" className="left flex-1 w-full">
         <p className=" text-gray-700 font-medium w-[100%]">
