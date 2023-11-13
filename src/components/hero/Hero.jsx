ff14cbcc13a560a0e0f1dd2529d0723001d0a37d
Siddharth goyal <siddharthgoyal198@gmail.com>
2023-11-13 13:23:32 -0700

    Bug Fix

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -17,6 +17,21 @@ const Hero = () => {
       <div className=" container mx-auto pt-5 h-[750px] md:h-[100vh] md:flex-col-reverse sm:h-[780px]  flex sm:flex-col-reverse sm:pt-0 ">
         <div className=" left mt-4 md:mt-0 flex-1 flex flex-col justify-center gap-5 w-1/2 md:w-full md:py-2 sm:py-0">
           <div className="info w-fit flex flex-col items-start justify-center gap-3 sm:gap-2">
+          <div className="right  top-5 flex-1 flex items-center justify-center md:items-end sm:items-end">
+          <div className="relative h-[88%] w-fit flex items-center sm:items-end">
+            <img
+            data-aos="fade-up"
+              className=" h-[50%]  w-full object-cover md:h-[55%] md:m-auto sm:m-0"
+              src='https://i.postimg.cc/DZ1ZZDt9/edit-1.jpg'
+              // src={mine}
+              alt="mine"
+            />
+           
+                
+              </div>
+            </div>
+            
+            
             <h2
               data-aos="fade-up"
               className=" text-5xl font-bold sm:text-[2rem]"
@@ -109,13 +124,7 @@ const Hero = () => {
               // src={mine}
               alt="mine"
             />
-            {/* <div className=" absolute bottom-10 md:bottom-3 right-8 md:right-2"> */}
-              {/* <div data-aos="zoom-in" data-aos-duration="1000"  className=" relative cursor-pointer"> */}
-                {/* <img */}
-                  {/* className=" w-[135px] md:w-[90px] circle-text" */}
-                  {/* // src="https://ik.imagekit.io/imgkitt/tr:w-400/Full_Stack_Developer2.png?updatedAt=1683134009107" */}
-                  {/* alt="" */}
-                {/* /> */}
+           
                 
               </div>
             </div>
